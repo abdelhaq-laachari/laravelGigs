26d26732e4c6b59e6b1a076463d8e67d0df64bb5
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2023-06-07 11:06:05 +0100

    feat: create new model called Listing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts/{id}',function($id){
+    return "This is post number ".$id;
+});
